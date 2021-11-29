fb30c033d66c60eef09211a70c9ca1a2829d9db6
Christian Sinai <christian_sinai@me.com>
2021-11-29 15:27:53 -0800

    3.1.6 update: included operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,25 @@
-function fight() {
-  window.alert('The fight has begun!');
-}
-
 const playerName = window.prompt('What is your robot\'s name?');
+let playerHealth = 100;
+let playerAttack = 10;
+
 console.log('Our robot\'s name is:', playerName);
+
+const enemyName = 'Roborto';
+let enemyHealth = 50;
+let enemyAttack = 12;
+
+const fight = function() {
+  window.alert('Welcome to Robot Gladiators!');
+
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName + 'attacked' + enemyName + '.' + enemyName + 'now has' + enemyHealth + 'health remaining.'
+  );
+
+  playerHealth = playerHealth - enemyAttack
+  console.log(
+    enemyName + 'attacked' + playerName + '.' + playerName + 'now has' + playerHealth + 'health remaining.'
+  );
+}
 
 fight();
